a159194c8dd202fa7de0969e47b5836c08119f3e
CedricMrt <cjyiphone@gmail.com>
2024-08-26 08:30:12 +0200

    feat: :sparkles: debut du front-end, page acceuil terminée

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\Categories;
 use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
+Route::get('/categories', Categories::class);
