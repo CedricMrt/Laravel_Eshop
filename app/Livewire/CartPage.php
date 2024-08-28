332996d1ed84673dac79b0053080bb1e93720a35
CedricMrt <cjyiphone@gmail.com>
2024-08-28 18:33:04 +0200

    feat: :sparkles: ajout dynamique sur la page d'acceuil des marques et categories

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Cart - Laravel Eshop')]
 class CartPage extends Component
 {
     public function render()
--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Home - Laravel Eshop')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', true)->get();
+        $categories = Category::where('is_active', true)->get();
+        return view('livewire.home-page', ['brands' => $brands, 'categories' => $categories]);
     }
 }
